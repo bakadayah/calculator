5d656523c65545df756bbead36a2224663548dcf
bakadayah <bakadayah@gmail.com>
2021-10-05 20:16:45 +1000

    Calculator Files
    Added calculator files

---
+++ HelloWorld.js
@@ -0,0 +1,5 @@
+function helloWorld(name){
+    console.log("Hello " + name + " World")
+
+}
+helloWorld("Michael's");
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+function getID(btn){
+    alert(btn.id);
+}
+function getValue(num){
+    document.form.text-view.value = document.form.text-view.value + num
+}
